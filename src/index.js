7c157c21f8a1fdf27758c05f1714ce91664996de
Jennesy Dai <jennesy.dai@gmail.com>
2025-07-29 15:48:00 +0800

    feat: add CORS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 require('module-alias/register');
 require('dotenv').config();
 const express             = require('express');
+const cors                = require('cors');
 const connectDB           = require('~/config/db');
 const seatRoutes          = require('~/routes/seatRoutes');
 const reservationRoutes   = require('~/routes/reservationRoutes');
 
 const app = express();
+
+// CORS configuration
+const isDev = process.env.NODE_ENV !== 'production';
+const CLIENT_ORIGINS = isDev ? '*' : process.env.CLIENT_ORIGINS?.split(',') || [];
+
+app.use(cors({
+  origin: CLIENT_ORIGINS,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 connectDB();
